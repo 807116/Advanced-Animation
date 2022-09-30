daed28001664aa994be579400d7bd5dfd9725970
807116 <807116@seq.org>
2022-09-30 12:44:55 -0700

    Vector Version complete

--- Lab 922 Orbit2/Planet.js
+++ Lab 922 Orbit2/Planet.js
@@ -1,29 +1,59 @@
-function Planet(x, y, d, n) {
-   this.loc
-   this.vel
-   this.acc
-   this.rad
-   this.numOrbit = n
-   this.orbs = [];
-
-
+function Planet(x, y, d, num) {
+    this.loc = new JSVector(x, y);
+    this.vel = new JSVector(Math.random() *4 -2, Math.random()*4 -2);
+    this.acc = new JSVector(0, 0);
+    this.diam = d;
+    this.orbs = [];
+    for(let j = 0; j<numPlanets; j++){
+        let angVell = Math.random()*.1 + .01;
+        for(let i = 0; i< num; i++){
+            this.orbs[i] = new Orbiter(10, (Math.PI*2/num)*i, angVell , this, 50);
+        }
+        
+    }
+    
+    
 }
 
 Planet.prototype.run = function () {
+    for(let i=0; i<this.orbs.length; i++){
+        this.orbs[i].run(this.loc.x, this.loc.y);
+    }
     this.render();
     this.update();
-    this.translate();
+    this.edges();
 }
 
 Planet.prototype.render = function () {
-   
+    context.save();
+    context.translate(this.loc.x, this.loc.y);
+    context.beginPath(); 
+    context.moveTo(20,0);
+    context.lineTo(-10,-20);
+    context.lineTo(-10,20);
+    context.lineTo(20,0);
+    context.fillStyle = "gray";    
+    context.fill();     
+    context.stroke();   
+    context.restore();
 }
 
 Planet.prototype.update = function () {
-
+    this.loc.add(this.vel);
 
 }
 
-Planet.prototype.translate = function () {
-   
+Planet.prototype.edges = function () {
+    if (this.loc.y < 0) {
+        this.loc.y = canvas.height;
+    }
+    if (this.loc.y > canvas.height) {
+        this.loc.y = 0;
+    }
+    if (this.loc.x > canvas.width) {
+        this.loc.x = 0;
+    }
+    if (this.loc.x < 0) {
+        this.loc.x = canvas.width;
+    }
 }
--- Lab 922 Orbit2/orbiter.js
+++ Lab 922 Orbit2/orbiter.js
@@ -1,26 +1,32 @@
-function Orbiter(x, y, d) {
-    this.loc
-    this.angVel
-    this.angle
-    this.orbRadius
-    this.rad
+function Orbiter(d, a, v, p, o){
+    this.diam=d;
+    this.ang =a;
+    this.angVel = v;
+    this.planet = p;
+    this.orbRad=o;
+    this.rotator = new JSVector();
+    this.rotator.setMagnitude(this.orbRad);
+    this.rotator.setDirection(this.ang);
+
 }
 
-Orbiter.prototype.run = function () {
+Orbiter.prototype.run = function(x, y){
     this.render();
-    this.update();
-    this.translate();
+    this.update(x, y);
 }
 
-Orbiter.prototype.render = function () {
-  
+Orbiter.prototype.render = function(){
+    let orbLoc = JSVector.addGetNew(this.planet.loc, this.rotator);
+    context.beginPath();
+    context.arc(orbLoc.x, orbLoc.y, this.diam/2, 0, 2*Math.PI);
+    context.strokeStyle = "black";
+    context.fillStyle = "#F8694B";
+    context.fill();
+    context.stroke(); 
+
 }
 
-Orbiter.prototype.update = function () {
-
-
-}
-
-Orbiter.prototype.translate = function () {
-   
+Orbiter.prototype.update = function(x, y){
+    this.rotator.rotate(this.angVel);
+    
 }
