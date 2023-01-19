1678999fe0177022f71181981dbc794fb1c5697d
807116 <807116@seq.org>
2023-01-19 12:52:20 -0800

    day 1

--- Lab 125 2D platformer/foreground.js
+++ Lab 125 2D platformer/foreground.js
@@ -1,5 +1,4 @@
 function Foreground(x, y, clr) {
-    //x should roughly 2/3 that of the background I guess?
     this.loc = new JSVector(x, y);
     this.vel = new JSVector(1, 0);
     this.clr = clr;
@@ -15,17 +14,15 @@ Foreground.prototype.run = function () {
 
 }
 Foreground.prototype.parallax = function () {
-    dx = this.loc.x+400;
+   
     if (this.loc.x >= 800) {
         this.loc.x = -800
     }
-    if (dx >= 800) {
-        dx = -800;
-    }
+   
 }
 Foreground.prototype.render = function () {
     let ctx = context;
-    dx = this.loc.x+400;
-    ctx.fillStyle = this.clr;
-    ctx.fillRect(this.loc.x, this.loc.y, dx, this.loc.y+600);
+    ctx.drawImage(background, 0, 0);
+
+    
 }
