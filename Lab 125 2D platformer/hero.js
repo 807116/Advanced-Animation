851113e0d19362a06b2f176c6bd82c3326928c9a
Evan Demas <112122380+807116@users.noreply.github.com>
2023-01-24 18:04:55 -0800

    changes

--- Lab 125 2D platformer/hero.js
+++ Lab 125 2D platformer/hero.js
@@ -1,6 +1,7 @@
 function Hero(width) {
     this.loc = new JSVector(100, 100);
-    this.vel = new JSVector(0, -1);
+    this.vel = new JSVector(0, 1);
+    this.acc = new JSVector(0,0);
     this.clr = "blue";
     this.size = width;
     this.height = 5
@@ -12,7 +13,7 @@ Hero.prototype.run = function () {
     this.render();
     this.update();
     this.checkPlatform();
-    this.checkDeath();
+    //this.checkDeath();
 
 }
 
@@ -24,7 +25,10 @@ Hero.prototype.update = function(){
 
 Hero.prototype.checkPlatform = function(){
     for(let i=0; i<platforms.length; i++){
-        if(this.loc.y >= platforms[i].loc.y){
+        // if(this.loc.y >= platforms[i].loc.y && ((this.loc.x >= platforms[i].loc.x) && (this.loc.x >= platforms[i].size+platforms[i].loc.x))){
+        //     this.vel.y = 0;
+        // }
+        if(this.loc.y >= platforms[i].loc.y && ((this.loc.x >= platforms[i].loc.x) && (this.loc.x >= platforms[i].size+platforms[i].loc.x))){
             this.vel.y = 0;
         }
     }
--- Lab 125 2D platformer/main.js
+++ Lab 125 2D platformer/main.js
@@ -17,13 +17,14 @@ function init() {
     foregrounds[0] = new Foreground(0,0, "Red");
     foregrounds[1] = new Foreground(-canvas.width/2, 0, "Blue");
     hero = new Hero(5);
-    loadPlatforms(10);
+    loadPlatforms(1);
     animate();
 }
 
 function loadPlatforms(n){
     for(let i = 0; i<n; i++){
-        platforms[i] = new Platform(Math.random() * (canvas.width), Math.random() * (500-300)+300,50);
+        //platforms[i] = new Platform(Math.random() * (canvas.width), Math.random() * (500-300)+300,50);
+        platforms[i] = new Platform(75, 300, 50);
     }
 }
 
