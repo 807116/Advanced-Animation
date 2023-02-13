9a727d33d2a3e53f0554f529cd5c76290df3bab4
Evan Demas <112122380+807116@users.noreply.github.com>
2023-02-13 10:38:58 -0800

    :(

--- Lab 0207 edemas/gamearea.js
+++ Lab 0207 edemas/gamearea.js
@@ -1,44 +1,60 @@
 function GameArea(){
-    //  Wrapper Div
-    this.wrapperDiv = document.getElementById("wrapperDiv");
-    this.wrapperDiv.setAttribute("style", " background-color:yellow; border: 5px solid black; width:1100px; height:800px;");
-    // create tileMenuDiv
-    this.tileMenuDiv = document.createElement("div");
-    this.wrapperDiv.appendChild(this.tileMenuDiv)
-    this.tileMenuDiv.setAttribute("style", " background-color:#FFFFFF; width:1100px; height:100px;float:left;");
+//  Wrapper Div
+this.wrapperDiv = document.getElementById("wrapperDiv");
+this.wrapperDiv.setAttribute("style", " background-color:yellow; border: 5px solid black; width:1300px; height:800px;");
+//left tile menu div
+this.tileMenuDivLeft = document.createElement("div");
+this.wrapperDiv.appendChild(this.tileMenuDivLeft)
+this.tileMenuDivLeft.setAttribute("style", "background-color:#FFFFFF; width:100px; height:800px;float:left;");
+// create tileMenuDiv
+this.tileMenuDiv = document.createElement("div");
+this.wrapperDiv.appendChild(this.tileMenuDiv)
+this.tileMenuDiv.setAttribute("style", " background-color:#FFFFFF; width:1100px; height:100px;float:left;");
+//right tile menu div
+this.tileMenuDivRight = document.createElement("div");
+this.wrapperDiv.appendChild(this.tileMenuDivRight)
+this.tileMenuDivRight.setAttribute("style", " background-color:#FFFFFF; width:100px; height:800px;float:right;");
 
-    // create canvasDiv
-    this.canvasDiv = document.createElement("div");
-    this.wrapperDiv.appendChild(this.canvasDiv)
-    this.canvasDiv.setAttribute("style", " background-color:pink; width:1100px; height:700px;float:left;");
+// create canvasDiv
+this.canvasDiv = document.createElement("div");
+this.wrapperDiv.appendChild(this.canvasDiv)
+this.canvasDiv.setAttribute("style", " background-color:pink; width:1100px; height:700px;float:left;");
+
+// place canvas in div and style
+this.canvasDiv.appendChild(canvas);
+//  create tiles for tile menu
+this.tiles = [];
+this.tileText = [];
+//top tiles
+for (let i = 0; i < 6; i++) {
+  this.tiles[i] = document.createElement("div");
+  this.tileMenuDiv.appendChild(this.tiles[i]);
+  this.tiles[i].setAttribute("class", "tile");
+  this.tileText[i] = document.createTextNode("Tile " + (i + 1) + "");
+  //this.t1Text.style.padding = "10px";
+  this.tiles[i].appendChild(this.tileText[i]);
+}
 
 
-    this.tileMenuDivRight = document.createElement("div");
-    this.wrapperDiv.appendChild(this.tileMenuDivRight)
-    this.tileMenuDivRight.setAttribute("style", " background-color:#FFFFFF; width:100px; height:800px;float:right;");
-
-    // place canvas in div and style
-    this.canvasDiv.appendChild(canvas);
-    //this.canvasDivRight.appendChild(canvas);
-    //  create tiles for tile menu
-    this.tiles = [];
-    this.tileText = [];
-    for(let i = 0; i < 6; i++){
-       this.tiles[i] = document.createElement("div");
-       this.tileMenuDiv.appendChild(this.tiles[i]);
-       this.tiles[i].setAttribute("class", "tile");
-       this.tileText[i] = document.createTextNode("Tile " + (i + 1) + "");
-       //this.t1Text.style.padding = "10px";
-       this.tiles[i].appendChild(this.tileText[i]);
-    }
-    for(let i = 6; i < 12; i++){
-      this.tiles[i] = document.createElement("div");
-      this.tileMenuDivRight.appendChild(this.tiles[i]);
-      this.tiles[i].setAttribute("class", "tile");
-      this.tileText[i] = document.createTextNode("Tile " + (i + 1) + "");
-      //this.t1Text.style.padding = "10px";
-      this.tiles[i].appendChild(this.tileText[i]);
-   }
+for (let i = 6; i < 12; i++) {
+  this.tiles[i] = document.createElement("div");
+  this.tileMenuDivRight.appendChild(this.tiles[i]);
+  this.tiles[i].setAttribute("class", "tile");
+  this.tileText[i] = document.createTextNode("Tile " + (i + 1) + "");
+  //this.t1Text.style.padding = "10px";
+  this.tiles[i].appendChild(this.tileText[i]);
+}
+this.tilesL = [];
+this.tileTextL = [];
+for (let i = 12; i < 18; i++) {
+  this.tilesL[i] = document.createElement("div");
+  this.tileMenuDivL.appendChild(this.tilesL[i]);
+  this.tilesL[i].setAttribute("class", "tileV");
+  this.tileTextL[i] = document.createTextNode("Tile " + (i + 1) + "");
+  this.tilesL[i].appendChild(this.tileTextL[i]);
+}
+  
+  
 
     //  Add listeners to tile objects
     for(let i = 0; i < this.tiles.length; i++){
